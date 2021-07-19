2c61007611b2fe7274538bd92cc2c937a360da24
sarav <sarav@DESKTOP-PRGSQL3>
2021-07-19 12:21:08 +0530

    done

--- javaassignment1/src/javaassignment1/even_odd.java
+++ javaassignment1/src/javaassignment1/even_odd.java
@@ -1,5 +1,18 @@
 package javaassignment1;
 
+import java.util.*;
+
 public class even_odd {
+	public static void main(String[] args) {
+		Scanner ss = new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int num = ss.nextInt();
+		
+		if(num%2 == 0)
+			System.out.println("The num is even");
+		else
+			System.out.println("The num is odd");
+		
+	}
 
 }
