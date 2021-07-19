80e4beaec0ce724f61e104c1368734aac02afebc
sarav <sarav@DESKTOP-PRGSQL3>
2021-07-19 13:51:52 +0530

    largest of 3 num

--- javaassignment1/src/javaassignment1/big_of_3num.java
+++ javaassignment1/src/javaassignment1/big_of_3num.java
@@ -3,20 +3,16 @@ import java.util.*;
 
 public class big_of_3num 
 {
-	static int max(int a, int b, int c)
-	{
-		return c > (a > b ? a : b) ? c : ((a > b) ? a : b);
-	}
 
 	public static void main(String[] args) 
 	{
-		int x, y, z, big;
+		int a, b, c, big;
 		Scanner ss = new Scanner(System.in);
 		System.out.println("enter 3 numbers");
-		x = ss.nextInt();
-		y = ss.nextInt();
-		z = ss.nextInt();
-		big = max(x, y, z);
+		a = ss.nextInt();
+		b = ss.nextInt();
+		c = ss.nextInt();
+		big = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
 		System.out.println(big + "is the largest num");	
 	}
 
